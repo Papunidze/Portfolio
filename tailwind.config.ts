364864f433a2b6cca50e2b5a11fa53d12158b9a7
papunidze <papunidze07@gmail.com>
2024-10-26 12:54:13 +0400

    feat:layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,51 @@
-import type { Config } from "tailwindcss";
+import { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+export default {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          light: "#B8390E",
+          DEFAULT: "#DC4731",
+          dark: "#3B0918",
+        },
+        secondary: {
+          light: "#F9F9FB",
+          DEFAULT: "#FFFFF5",
+          dark: "#F1F1F1",
+        },
+        backgrounds: {
+          primary: "#fff",
+          secondary: "#fafafa",
+        },
+        typography: {
+          primary: "rgb(23, 23, 23)",
+          secondary: "rgb(102, 102, 102)",
+        },
+        border: {
+          primary: "rgb(235, 235, 235)",
+          secondary: "rgb(201, 201, 201)",
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
       },
     },
+
+    backgroundImage: {
+      "gradient-1": "linear-gradient(to top left, #DC4731, #3B0918)",
+      "gradient-2": "linear-gradient(to top left, #1B314D, #2C4669)",
+      "gradient-3": "linear-gradient(to top left, #FEFBF5, #edf2fb)",
+      "gradient-4": "linear-gradient(to top left, #d7e3fc, #FEFBF5)",
+    },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [typography],
+} satisfies Config;
